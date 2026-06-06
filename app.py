bd90ed33e872442c945bc502dfaa8998bc688451
shivam1108-06 <shivamkotak118@gmail.com>
2026-06-07 00:30:34 +0530

    Added QR code generation

--- app.py
+++ app.py
@@ -1,10 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import qrcode
+import os
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return render_template("index.html")
+
+    qr_image = None
+
+    if request.method == "POST":
+
+        data = request.form["data"]
+
+        img = qrcode.make(data)
+
+        if not os.path.exists("static"):
+            os.makedirs("static")
+
+        img.save("static/qrcode.png")
+
+        qr_image = "qrcode.png"
+
+    return render_template(
+        "index.html",
+        qr_image=qr_image
+    )
 
 if __name__ == "__main__":
     app.run(debug=True)
